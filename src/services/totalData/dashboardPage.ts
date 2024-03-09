85102deedde0a3665eaa265947a249323d2def2e
Smit9313 <smitdudhat10@gmail.com>
2024-03-09 17:20:10 +0530

    minor changes

--- src/services/totalData/dashboardPage.ts
+++ src/services/totalData/dashboardPage.ts
@@ -1,7 +1,6 @@
 import { ITotalDataResponse } from '../../interfaces/totalData/ItotalData';
 
 export const formateTotalDataResponse = (response: ITotalDataResponse) => {
-	console.log(response)
   const updatedResponse = response?.data
   return updatedResponse;
 }
