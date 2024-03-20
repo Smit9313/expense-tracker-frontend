f14afa8f6eb2a2431e5d31ffdf0f1c2ac49bb8dd
Smit9313 <smitdudhat10@gmail.com>
2024-03-20 14:30:29 +0530

    pwa changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {VitePWA} from "vite-plugin-pwa"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({
+    manifest: {
+      icons: [
+        {
+          src: "/budget.jpg",
+          sizes: "512x512",
+          type: "image/jpg",
+          purpose: "any maskable"
+        }
+      ]
+    }
+  })],
 })
